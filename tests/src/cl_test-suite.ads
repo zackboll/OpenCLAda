3415e5a3d02935ddbe20f972446d40e1d1617941
Zack Boll <zboll@ryzen9>
2025-08-30 22:41:25 -0400

    wip, add AUnit dependency to crate

---
+++ tests/src/cl_test-suite.ads
@@ -0,0 +1,5 @@
+with AUnit.Test_Suites;
+
+package CL_Test.Suite is
+  function Get return AUnit.Test_Suites.Access_Test_Suite;
+end CL_Test.Suite;
---
+++ tests/src/tests.adb
@@ -0,0 +1,11 @@
+with Ada.Text_IO;
+with AUnit.Run;
+with AUnit.Reporter.Text;
+
+procedure Tests is
+
+  --  procedure Run is new AUnit.Run.Test_Runner ();
+
+begin
+   Ada.Text_IO.Put_Line ("Hello, World!");
+end Tests;
